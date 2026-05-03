0b0aaf6048c2790f1a68e19d4545044bafe89e3b
David Cohen <dmc13561356@gmail.com>
2026-05-04 00:44:42 +0300

    Add url front path

--- src/main/java/com/example/mom_birthday_site/controller/MainController.java
+++ src/main/java/com/example/mom_birthday_site/controller/MainController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173", "http://localhost:3001"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173", "http://localhost:3001", "https://happy-birthday-qj6e.onrender.com/"})
 @RestController
 @RequestMapping("/api")
 public class MainController {
